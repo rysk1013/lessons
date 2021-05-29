fc5fa58fe6d7a210a7efc161d57752d646cbd527
rysk1013 <rysk2908@gmail.com>
2021-05-29 18:24:03 +0900

    変数宣言・const

--- lesson1.go
+++ lesson1.go
@@ -1,9 +1,36 @@
+// 変数宣言・const
+
 package main
 
-import (
+import "fmt"
 
+const Pi = 3.14
+
+const (
+	Username = "test_user"
+	Password = "test_pass"
 )
 
+var (
+	i int = 1
+	f64 float64 = 1.2
+	s string = "text"
+	t, f bool = true, false
+)
+
+func foo() {
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println(xi, xf64, xs, xt, xf)
+	fmt.Printf("%T\n", xf64)
+}
+
 func main() {
-	
+	fmt.Println(i, f64, s, t, f)
+
+	foo()
+
+	fmt.Println(Pi, Username, Password)
 }
