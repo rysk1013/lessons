ec4f6fd22c3e48b154e9d75797296c24cba4c67e
rysk1013 <rysk2908@gmail.com>
2021-06-01 23:56:36 +0900

    Stringer

---
+++ lesson18.go
@@ -0,0 +1,18 @@
+// Stringer
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("My name is %v", p.Name)
+}
+
+func main() {
+	mike := Person{"Mike", 22}
+	fmt.Println(mike)
+}
